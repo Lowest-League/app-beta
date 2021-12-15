8351f4ae47eb484b63103a0016177a0e535edbc4
cl4pper <cl4pper@gmail.com>
2021-12-15 16:51:43 +0000

    chore: setup app

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-const components = require.context('@components', true, /\.story\.js$/);
-
-function loadStories() {
-	components.keys().forEach((filename) => components(filename));
-}
-
-configure(loadStories, module);
---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    stories: ["../src/components/**/*.story.jsx"],
+    addons: ["@storybook/addon-postcss"],
+	webpackFinal: async (config) => {
+		config.resolve.alias = {
+			'@styles': path.resolve(__dirname, '../src/styles')
+		};
+
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: [
+				{ loader: 'style-loader' },
+				{ loader: 'css-loader' },
+				{ loader: 'sass-loader', options: { additionalData: '@import "~@styles/global.scss";' } }
+			],
+			include: path.resolve(__dirname, "../")
+		  });
+
+		  return config;
+	}
+};
