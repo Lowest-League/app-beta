89e5aa4e947363c912c9d31467dd2bcc5440fbc5
cl4pper <cl4pper@gmail.com>
2022-02-13 16:26:32 +0000

    chore: setup storybook aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,9 @@ module.exports = {
 			'@icons': path.resolve(__dirname, '../src/assets/icons'),
 			'@components': path.resolve(__dirname, '../src/components'),
 			'@containers': path.resolve(__dirname, '../src/containers'),
+			'@api': path.resolve(__dirname, '../src/api'),
+			'@store': path.resolve(__dirname, '../src/store'),
+			'@store/*': path.resolve(__dirname, '../src/store/*'),
 		};
 
 		config.module.rules.push({
