b906ad74748b8cba186f50528a64ddc57537b98e
cl4pper <cl4pper@gmail.com>
2022-03-01 19:55:22 +0000

    feat: add RightIcon to assets

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,4 @@
 export { default as FootballLogo } from './football';
 export { default as GithubLogo } from './github';
 export { default as CloseIcon } from './close';
+export { default as RightIcon } from './right';
